4d8e0850212a925e959ec180417763c811f31bc3
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-09-21 15:40:39 +0200

    Create M - Capital or Small or Digit.py

---
+++ Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/M - Capital or Small or Digit.py
@@ -0,0 +1,11 @@
+x = input()
+
+if x.isdigit():
+    print('IS DIGIT')
+
+elif x.isalpha():
+    print('ALPHA')
+    if x.isupper():
+        print('IS CAPITAL')
+    else:
+        print('IS SMALL')
