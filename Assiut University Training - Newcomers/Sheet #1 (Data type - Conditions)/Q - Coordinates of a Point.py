41c1054c3a0b468299f78e6c80f2a0efd4087c5a
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-09-21 15:42:44 +0200

    Update Q - Coordinates of a Point.py

--- Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/Q - Coordinates of a Point.py
+++ Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/Q - Coordinates of a Point.py
@@ -1,1 +1,23 @@
+coordinates  = list(map(float, input().split()))
 
+if (coordinates[0] == 0) and (coordinates[1] == 0):
+    print('Origem')
+
+elif (coordinates[0] != 0) and (coordinates[1] == 0):
+    print('Eixo X')
+
+elif (coordinates[0] == 0) and (coordinates[1] != 0):
+    print("Eixo Y")
+
+else:
+    if (coordinates[0] > 0) and (coordinates[1] > 0):
+        print('Q1')
+
+    elif (coordinates[0] < 0) and (coordinates[1] < 0):
+        print('Q3')
+
+    elif (coordinates[0] < 0) and (coordinates[1] > 0):
+        print('Q2')
+
+    elif (coordinates[0] > 0) and (coordinates[1] < 0):
+        print('Q4')
