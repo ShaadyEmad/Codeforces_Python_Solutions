cfe8e8bc0fd8790f7183cfcf4a8ef3c9e6303b33
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-09-23 02:33:29 +0200

    Update A - Winter Sale.py

--- Assiut University Training - Newcomers/Contest #1/A - Winter Sale.py
+++ Assiut University Training - Newcomers/Contest #1/A - Winter Sale.py
@@ -1,4 +1,4 @@
-discount, price_afer = map(int,input().split())
+discount, price_after = map(int,input().split())
 discount = 100 - discount
-price_before = round(100*price_afer/discount,2)
+price_before = round(100*price_after/discount,2)
 print("{:.2f}".format(price_before))
