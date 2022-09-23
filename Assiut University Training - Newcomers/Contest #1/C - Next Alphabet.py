6c3ec79c88103a14b1819738dbeba7ab991ed19c
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-09-23 02:50:54 +0200

    Create C - Next Alphabet.py

---
+++ Assiut University Training - Newcomers/Contest #1/C - Next Alphabet.py
@@ -0,0 +1,10 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+char = input()
+
+index = alphabet.index(char)
+
+if index == 25:
+    print(alphabet[0])
+else:
+    print(alphabet[index+1])
